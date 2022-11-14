357052ac89e41e7b2c1370b85ee875287c491ec4
teddu0 <107142125+teddu0@users.noreply.github.com>
2022-11-14 20:08:01 +0300

    Поправил фикстуру

--- src/test/java/tests/PostCreationTests.java
+++ src/test/java/tests/PostCreationTests.java
@@ -1,14 +1,16 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
 public class PostCreationTests extends TestBase {
+    Faker faker = new Faker();
 
     @Test
     public void testTextPostCreation() throws InterruptedException {
         app.getNavigationHelper().goToProfile();
         Thread.sleep(2000);
-        app.getPostCreationHelper().fillPostField("Hello people");
+        app.getPostCreationHelper().fillPostField(faker.gameOfThrones().house());
         Thread.sleep(2000);
         app.getPostCreationHelper().submitPostCreation();
         Thread.sleep(3000);
